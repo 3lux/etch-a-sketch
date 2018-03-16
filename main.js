18c530eb240d51cdca2b8fc6a553ec4bf985b2be
Sean <seanfindlay3@gmail.com>
2018-03-16 19:09:29 +0000

    Add cell fill

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 const container = document.querySelector('#container');
+var numCells;
 
-for (i = 0; i < 16; i++) {
-	for (let x = 0; x < 16; x++) {
-		const cellSize = 512 / 16;
-		const cell = document.createElement('div');
+function resetCells(){
+	while (container.hasChildNodes()) {
+		container.removeChild(container.childNodes[0]);
+	}
+	numCells = prompt("Grid size");
+	createGrid(numCells);
+}
 
-		cell.style.border = "1px solid #000";
-		cell.setAttribute('class', 'cell');
-		cell.style.width = cellSize + 'px';
-		cell.style.height = cellSize + 'px';
-		container.appendChild(cell);
-    }
-};
+function createGrid(cellNum){
+	for (i = 0; i < cellNum; i++) {
+		for (let x = 0; x < cellNum; x++) {
+			const cellSize = 512 / cellNum;
+			const cell = document.createElement('div');
+
+			cell.style.border = "1px solid #000";
+			cell.setAttribute('class', 'cell');
+			cell.style.width = cellSize + 'px';
+			cell.style.height = cellSize + 'px';
+			cell.setAttribute('onmouseenter', 'this.setAttribute("class", "filled")');
+			container.appendChild(cell);
+		}
+	};
+}
+
+createGrid(16);
